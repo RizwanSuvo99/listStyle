50c306a5578969e7a2260a1c81b4086ac06373a7
Md.Rizwan Uddin <45879960+RizwanSuvo99@users.noreply.github.com>
2020-06-21 16:29:48 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function subDropdown() {
+	var x = document.getElementById("subMenu");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function subDropdown2() {
+	var y = document.getElementById("subMenu2");
+	if (y.style.display === "none") {
+		y.style.display = "block";
+	} else {
+		y.style.display = "none";
+	}
+}
